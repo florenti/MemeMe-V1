b39f8c94d14e7579f891e7abbec40a65e6744799
flospaMac <florent.spahiu@gmail.com>
2017-05-17 01:12:26 +0200

    firstComit
    initial commit

---
+++ MemeMe-V1/MemeMe-V1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe-V1
+//
+//  Created by Florent Spahiu on 17/05/2017.
+//  Copyright © 2017 Florent Spahiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
